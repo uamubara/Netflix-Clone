c6850e817b8658355bc38cf03a867b15511ef010
uamubara <u.mubarak@hotmail.com>
2021-09-02 19:50:00 -0500

    Added Firbase config

---
+++ src/firbase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBi4DTrOr9Wxx4pJ3gUgFvTS1668Rx8_FM",
+  authDomain: "netflix-clone-54aae.firebaseapp.com",
+  projectId: "netflix-clone-54aae",
+  storageBucket: "netflix-clone-54aae.appspot.com",
+  messagingSenderId: "578933157077",
+  appId: "1:578933157077:web:a47a63133c946dd085465c"
+};
